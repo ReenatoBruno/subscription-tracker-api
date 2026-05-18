f33e77d9e398f2988eb300d7e95aeb5df602662d
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-05-18 19:36:45 -0300

    feat: implement subscription model
    - add schema validation
    - add pre-save middleware

--- model/subscription.model.ts
+++ model/subscription.model.ts
@@ -3,11 +3,19 @@ import mongoose, { type Document, type Types } from "mongoose";
 interface ISubscription extends Document {
   name: string;
   price: number;
-  currency: string;
-  frequency: string;
-  category: string;
+  currency: "USD" | "EUR" | "BRL";
+  frequency: "Monthly" | "Quarterly" | "Semiannual" | "Yearly";
+  category:
+    | "Sports"
+    | "News"
+    | "Entertainment"
+    | "Lifestyle"
+    | "Technology"
+    | "Finance"
+    | "Politics"
+    | "others";
   paymentMethod: string;
-  status: string;
+  status: "Active" | "Canceled" | "Expired";
   startDate: Date;
   renewalDate: Date;
   user: Types.ObjectId;
@@ -88,6 +96,25 @@ const subscriptionSchema = new mongoose.Schema<ISubscription>(
   },
   { timestamps: true },
 );
+
+subscriptionSchema.pre("save", async function () {
+  if (!this.renewalDate) {
+    const renewalPeriods: { [key in ISubscription["frequency"]]: number } = {
+      Monthly: 30,
+      Quarterly: 90,
+      Semiannual: 180,
+      Yearly: 365,
+    };
+    const days = renewalPeriods[this.frequency];
+    if (days) {
+      this.renewalDate = new Date(this.startDate);
+      this.renewalDate.setDate(this.renewalDate.getDate() + days);
+    }
+  }
+  if (this.renewalDate < new Date()) {
+    this.status = "Expired";
+  }
+});
 
 const Subscription = mongoose.model<ISubscription>(
   "Subscription",
